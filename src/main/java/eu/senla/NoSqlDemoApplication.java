16e01d84f217a5974113ebab429663dbe280d5d9
Igor Klimov <igor_klimov@senla.eu>
2024-11-12 12:38:37 +0300

    base impl

---
+++ src/main/java/eu/senla/NoSqlDemoApplication.java
@@ -0,0 +1,11 @@
+package eu.senla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoSqlDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NoSqlDemoApplication.class, args);
+    }
+}
